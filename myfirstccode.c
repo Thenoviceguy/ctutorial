9a2c47e36b319221f62d8d6b6bf02980184962fe
Thenoviceguy <Thenoviceguy@github.com>
2023-10-04 13:51:50 +0700

    new branch

--- myfirstccode.c
+++ myfirstccode.c
@@ -7,7 +7,10 @@ y = 25;
 x = 123;
 int age = 21;//intenger
 float gpa=2.05;//floating point
+char grade = 'B';
 char name[] = "brogs";
-printf("you are %d",y);  
+printf("you are %d\n",y);
+printf("your gpa is %f\n",gpa);
+printf("your average grade is %c\n",grade);
 return 0;
 }
