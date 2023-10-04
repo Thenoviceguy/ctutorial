aa094094445c139ed3ab982ae909534af542cd25
Thenoviceguy <Thenoviceguy@github.com>
2023-10-04 08:35:13 +0700

    changed the name to value of y

--- myfirstccode.c
+++ myfirstccode.c
@@ -2,10 +2,12 @@
 
 int main (){
 int x;
+int y;
+y = 25;
 x = 123;
 int age = 21;//intenger
 float gpa=2.05;//floating point
 char name[] = "brogs";
-printf("you are %s",name);  
+printf("you are %d",y);  
 return 0;
 }
