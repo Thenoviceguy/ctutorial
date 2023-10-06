b98ad9663b4b8bd927de8686808e87c2095517d6
Thenoviceguy <Thenoviceguy@github.com>
2023-10-06 11:49:17 +0700

    added initial and added another char

--- myfirstccode.c
+++ myfirstccode.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 int main (){
 int x;
 int y;
@@ -7,11 +7,15 @@ y = 25;
 x = 123;
 int age = 21;//intenger
 float gpa=2.05;//floating point
-char name[] = "brogs";
+char e = 120;
+char name[] = "xrogs";
 char grade = 'B';
+bool east = true;
+
 
 printf("you are %d\n",y);
 printf("your average grade is %c\n",grade);
 printf("your name is %s\n",name);
+printf("your initial is %c\n",e);
 return 0;
 }
