ac0b7502bb17c250ef8f7d437f90d4c0b90cb5fa
Thenoviceguy <Thenoviceguy@github.com>
2023-10-08 15:08:40 +0700

    added an incomplete c file

---
+++ formatspecifiers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+// format specifers % = defines and formats a type of data to be displayed
+
+/*%c =character
+ %s = string
+ %f =float
+ %d integer
+
+ %.1 = decimal precisiom
+ %1 = minimum field width
+ %- left align*/
+
+return 0;
+}
