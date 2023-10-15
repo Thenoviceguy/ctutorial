f6e2da27b33e6c89fb4ced16339f466c60d332e5
Thenoviceguy <Thenoviceguy@github.com>
2023-10-15 14:06:25 +0700

    added another incomplete c file

---
+++ math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+
+//arithemethic operators
+  // + (addition)
+  // - (substraction)
+  // * (multiplication)
+  // / (division)
+  // ++ increment
+  // -- decrement
+  // % modulus
+
+}
